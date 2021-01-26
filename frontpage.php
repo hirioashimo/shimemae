561ceeffa93346fbab4d0b544de8e7b52832b0b9
ogixhiro <muraoka_hiro@ogix.co.jp>
2021-01-26 23:58:27 +0900

    フロントページ元ファイル

---
+++ frontpage.php
@@ -0,0 +1,627 @@
+<?php
+/**
+ *Template Name: Frontpage
+ *
+ *
+ * @package Screenr
+ */
+
+get_header(); ?>
+
+
+
+	<div id="content" class="site-content">
+		<main id="main" class="site-main" role="main">
+            
+			
+			
+			<section id="hero" class="section-slider screenr-section swiper-slider full-screen fixed" >	
+		<div class="swiper-container" data-autoplay="7000">
+		<div class="swiper-wrapper">
+
+
+
+				
+				
+							
+<div class="cp_cont">
+	<div class="cp_offcm01">
+		<input type="checkbox" id="cp_toggle01">
+		<label for="cp_toggle01"><span></span></label>
+		<div class="cp_menu">
+		<ul>
+		<li><a href="https://shimemae.com/">TOP</a></li>
+		<li><a href="https://shimemae.com/#nayami">こんなお悩みはありませんか？</a></li>
+		<li><a href="https://shimemae.com/#katamari">飲食店は財産の塊</a></li>
+		<li><a href="https://shimemae.com/#about">代表者挨拶</a></li>
+		<li><a href="https://shimemae.com/#support">助っ人</a></li>
+		<li><a href="https://shimemae.com/#faq">よくある質問</a></li>
+		<li><a href="https://shimemae.com/#news">新着情報</a></li>
+		<li><a href="https://shimemae.com/#contact">無料相談</a></li>
+		<li><a href="https://shimemae.com/company/">会社概要</a></li>
+		</ul>
+		</div>
+	</div>
+</div>
+			<div class="swiper-slide slide-align-center slide_content slide_content_layout_1" style="background-image: url('https://shimemae.com/wp-content/uploads/2020/12/topbg.jpg')"><div class="swiper-slide-intro"><div class="swiper-intro-inner"><p>
+				
+				<picture>
+  			<source media="(min-width: 600px)" srcset="https://shimemae.com/wp-content/uploads/2021/01/toppc.png">
+  			<img src="https://shimemae.com/wp-content/uploads/2021/01/top.png">
+		</picture>
+				
+				
+				
+				
+				
+				</p>
+</div></div><div class="overlay"></div></div>		</div>				
+					<div class="btn-next-section"></div>			
+		</div>			
+			</section>
+			<div class="mainimageunder" id="nayami">
+
+				<h2 class="maintitle">こんなお悩みはありませんか？</h2>
+			<picture>
+  			<source media="(min-width: 600px)" srcset="https://shimemae.com/wp-content/uploads/2021/01/topunder.jpg">
+  			<img src="https://shimemae.com/wp-content/uploads/2021/01/topundersp.jpg">
+		</picture>
+				
+</div>
+			
+
+<section class="section-contact section-padding onepage-section wg" id="katamari">
+<div class="container">	
+<div class="section-title-area">
+	<h2 class="section-title bagred">飲食店は財産の塊です。</h2>
+</div>
+<p class="message">お店は生き物であり、閉店してしまったら単なる空箱になってしまいます。<br>
+だから、閉店をする前がもっとも重要であり、その<span class="red textybg">財産の価値を生み出す</span>ことが大切なのです。<br>
+我々「シメマエ」チームはお店の価値を上げるお手伝いをする<br>
+	<span class="red">「最強の助っ人」</span>であります。<br>
+</p>
+
+<div class="section-title-area">
+<h2 class="section-title red space">『見えない価値を　　　　<br>　　　　見える価値に』</h2>
+</div>
+<div class="section-title-area">
+<h2 class="section-title red space">『あなたのお店には8つの価値があります』</h2>
+</div>
+<div class="section-title-area">
+<i class="fa fa-sort-down"></i>　　<i class="fa fa-sort-down"></i>　　<i class="fa fa-sort-down"></i>
+</div>
+	
+<div class="section-content services-content">
+				<div class="row">
+											<div class="col-sm-4">
+																<div class="card service__media top">
+										<img src="https://shimemae.com/wp-content/uploads/2021/01/zaisan01.jpg" alt="" loading="lazy" />										<div class="card-ig-overlay card-block">
+											<div class="service-card-content">
+												<h3 class="card-title">営業年数財産</h3>
+
+												<div class="card-text"><p>今までに沢山のお客様が来店されています</p>
+</div>
+											</div>
+																					</div>
+									</div>
+															</div>
+												<div class="col-sm-4">
+																<div class="card service__media top">
+										<img src="https://shimemae.com/wp-content/uploads/2021/01/zaisan02.jpg" class="attachment-screenr-service-small size-screenr-service-small wp-post-image" alt="" loading="lazy" />										<div class="card-ig-overlay card-block">
+											<div class="service-card-content">
+												<h3 class="card-title">来店客数財産</h3>
+
+												<div class="card-text"><p>お客様の口コミは一番の広告です。</p>
+</div>
+											</div>
+																					</div>
+									</div>
+															</div>
+												<div class="col-sm-4">
+																<div class="card service__media top">
+										<img src="https://shimemae.com/wp-content/uploads/2021/01/zaisan03.jpg" class="attachment-screenr-service-small size-screenr-service-small wp-post-image" alt="" loading="lazy" />										<div class="card-ig-overlay card-block">
+											<div class="service-card-content">
+												<h3 class="card-title">内装財産</h3>
+
+												<div class="card-text"><p>お洒落な内装は居抜き売却の強みとなります。</p>
+</div>
+											</div>
+																					</div>
+									</div>
+															</div>
+						</div><!-- /.row  -->
+	
+						
+						<div class="row">
+											<div class="col-sm-4">
+																<div class="card service__media top">
+										<img src="https://shimemae.com/wp-content/uploads/2021/01/zaisan04.jpg" class="attachment-screenr-service-small size-screenr-service-small wp-post-image" alt="" loading="lazy" />										<div class="card-ig-overlay card-block">
+											<div class="service-card-content">
+												<h3 class="card-title">備品財産</h3>
+
+												<div class="card-text"><p>厨房機器以外の調理道具・食器・家具・装飾品も財産に！</p>
+</div>
+											</div>
+																					</div>
+									</div>
+															</div>
+												<div class="col-sm-4">
+																<div class="card service__media top">
+										<img src="https://shimemae.com/wp-content/uploads/2021/01/zaisan05.jpg" class="attachment-screenr-service-small size-screenr-service-small wp-post-image" alt="" loading="lazy" />										<div class="card-ig-overlay card-block">
+											<div class="service-card-content">
+												<h3 class="card-title">保証金財産</h3>
+
+												<div class="card-text"><p>償却はされるものの確実に戻るお金です</p>
+</div>
+											</div>
+																					</div>
+									</div>
+															</div>
+												<div class="col-sm-4">
+																<div class="card service__media top">
+										<img src="https://shimemae.com/wp-content/uploads/2021/01/zaisan06.jpg" class="attachment-screenr-service-small size-screenr-service-small wp-post-image" alt="" loading="lazy" />										<div class="card-ig-overlay card-block">
+											<div class="service-card-content">
+												<h3 class="card-title">立地条件財産</h3>
+
+												<div class="card-text"><p>駅近、繁華街、駐車場完備など</p>
+</div>
+											</div>
+																					</div>
+									</div>
+															</div>
+						</div><!-- /.row  -->
+	
+	
+						<div class="row">
+											<div class="col-sm-4">
+																<div class="card service__media top">
+										<img src="https://shimemae.com/wp-content/uploads/2021/01/zaisan07.jpg" class="attachment-screenr-service-small size-screenr-service-small wp-post-image" alt="" loading="lazy" />										<div class="card-ig-overlay card-block">
+											<div class="service-card-content">
+												<h3 class="card-title">技術財産</h3>
+
+												<div class="card-text"><p>売りとする料理など</p>
+</div>
+											</div>
+																					</div>
+									</div>
+															</div>
+							
+							
+							
+							<div class="col-sm-8">
+																<div class="card service__media top">
+										<img src="https://shimemae.com/wp-content/uploads/2021/01/zaisan08.jpg" class="attachment-screenr-service-small size-screenr-service-small wp-post-image" alt="" loading="lazy" />										<div class="card-ig-overlay card-block">
+											<div class="service-card-content">
+												<h3 class="card-title">スタッフ財産</h3>
+
+												<div class="card-text"><p>そして何よりも長年ともにお店を切り盛りしているスタッフ財産なのです！</p>
+</div>
+											</div>
+																					</div>
+									</div>
+															</div>
+							
+												
+						</div><!-- /.row  -->
+<div class="row">
+
+				</div>
+			</div><!-- /.section-content -->	
+
+	
+<div class="section-title-area">
+	<?php
+  echo do_shortcode('[su_animate type="bounceIn" duration="1" delay="0" inline="no" class=""]<img src="https://shimemae.com/wp-content/uploads/2021/01/contents2.png" alt="だから生きている閉める前が大切なのです！">[/su_animate]		');
+?>
+	
+
+
+</div>
+
+</div>
+</section>	
+
+<section class="section-contact section-padding onepage-section yg">
+<div class="container">
+		<?php
+  echo do_shortcode('[su_animate type="wobble" duration="1" delay="0" inline="no" class=""]
+  <a href="#contact"><img src="https://shimemae.com/wp-content/uploads/2021/01/bngr.png" alt="だから店を閉めたい 無料相談はコチラ"></a>
+  
+   [/su_animate]		');
+?>
+</div>
+</section>	
+			
+<section class="section-contact section-padding onepage-section wg">
+<div class="container">	
+	
+	<?php
+  echo do_shortcode('[su_animate type="bounceIn" duration="1" delay="0" inline="no" class=""]
+  <picture>
+<source media="(min-width: 600px)" srcset="https://shimemae.com/wp-content/uploads/2021/01/contents.png">
+<img src="https://shimemae.com/wp-content/uploads/2021/01/contentssp.jpg" alt="今まで何年もお店を経営してきた。それは社長の財産です。">
+</picture>
+  [/su_animate]		');
+?>
+		
+	
+</div>
+</section>
+			
+
+	
+<section class="section-contact section-padding onepage-section section-meta" id="about">
+<div class="container">
+<div class="section-title-area">
+<h2 class="section-title">代表者挨拶</h2>
+</div>
+<div class="section-content">
+
+<div class="messagebox">
+<h3>困っている社長様を助けたい</h3>
+<p class="message">今まで何年もお店を経営してきた。<br>
+<span class="red textybg">それはお店の財産です。</span><br>
+その財産を少しでも無駄にして頂きたくないとの思いで我々は、「シメマエ.com」を始動いたしました。<br>
+私達はそんな社長様たちを応援・支援することに本気で取り組みます。<br>
+<span class="red">必ず成功する閉店方法</span>があります。</p>
+
+<p class="name">代表　<span>三宅　寛昭</span></p>
+</div>
+	
+</div>
+</div>
+</section>
+			
+
+
+<section class="section-contact section-padding onepage-section yg">
+<div class="container">
+		<?php
+  echo do_shortcode('[su_animate type="wobble" duration="1" delay="0" inline="no" class=""]
+  <a href="#contact"><img src="https://shimemae.com/wp-content/uploads/2021/01/bngr.png" alt="だから店を閉めたい 無料相談はコチラ"></a>
+  
+   [/su_animate]		');
+?>
+</div>
+</section>	
+			
+<section class="section-contact section-padding onepage-section" id="support">
+<div class="container">
+<div class="section-title-area">
+<h2 class="section-title">助っ人<br>
+	<span class="mini">三宅 寛昭◆木下 真</span></h2>
+</div>
+<div class="section-content">
+	
+<ul class="timeline">
+  <li>
+    <p class="timeline-date">1994年</p>
+    <div class="timeline-content">
+      <h3>港区西麻布にてオーストラリアレストランバー「oz café」開店し事業承継</h3>
+		<p>日本初オープンテラスを取り入れる。<br>ＣＮＮヘッドライン取材</p>
+    </div>
+  </li>
+  <li>
+    <p class="timeline-date">1994年</p>
+    <div class="timeline-content">
+      <h3>オーストラリアゴールドコースト</h3>
+      <p>メキシカン料理「ラ　メックス」コンサルティング</p>
+    </div>
+  </li>
+  <li>
+    <p class="timeline-date">1999年</p>
+    <div class="timeline-content">
+      <h3>港区西麻布にて隠れ家和食店「真」開店し事業承継</h3>
+      <p>新しい創作和食を取り入れる。<br>
+ＴＶ「アド街ック天国」取材
+</p>
+    </div>
+  </li>
+	<li>
+    <p class="timeline-date">2000年</p>
+    <div class="timeline-content">
+      <h3>港区青山にて「すし泉」</h3>
+      <p>プロデュースコンサルティングを行う。</p>
+    </div>
+  </li>
+<li>
+    <p class="timeline-date">2000年</p>
+    <div class="timeline-content">
+      <h3>渋谷区神泉にて「神泉パーラー」</h3>
+      <p>コンサルティング</p>
+    </div>
+  </li>
+<li>
+    <p class="timeline-date">2001年</p>
+    <div class="timeline-content">
+      <h3>(株)伊藤忠商事グループ</h3>
+      <p>(株)リーテイルブランディングとの共同リニュアール事業　大型和食店町田「隠れ坊」
+</p>
+    </div>
+  </li>
+<li>
+    <p class="timeline-date">2002年</p>
+    <div class="timeline-content">
+      <h3>西麻布発 カラオケスナック「円盤」</h3>
+      <p>開店し事業承継</p>
+    </div>
+  </li>
+<li>
+    <p class="timeline-date">2002年</p>
+    <div class="timeline-content">
+      <h3>渋谷パルコ7F、8Fのレストランフロアーリニューアル</h3>
+      <p>株式会社パルコと共同計画を行う。
+同時に７Ｆフロアーにて「ビストロバー真」開店
+新しい創作洋食を取り入れる。
+</p>
+    </div>
+  </li>
+<li>
+    <p class="timeline-date">2003年</p>
+    <div class="timeline-content">
+      <h3>渋谷にて会員制すし店「すし家」</h3>
+      <p>開店し事業継承</p>
+    </div>
+</li>
+<li>
+    <p class="timeline-date">2003年</p>
+    <div class="timeline-content">
+      <h3>レストラン「dish」</h3>
+      <p>開店</p>
+    </div>
+</li>
+	
+<li>
+    <p class="timeline-date">2003年</p>
+    <div class="timeline-content">
+      <h3>渋谷東急文化会館プラネタリウム閉館イベント「meets」</h3>
+      <p>企画クラブ運営<br>
+(株)トランジットジェネラルオフィス　中村氏<br>
+レストラン「ＦＵＲＵＴＯＳＨＩ」古里氏<br>
+</p>
+    </div>
+</li>
+<li>
+    <p class="timeline-date">2003年</p>
+    <div class="timeline-content">
+      <h3>西麻布　レストランラウンジ「ニュイ」</h3>
+      <p>開店</p>
+    </div>
+</li>
+<li>
+    <p class="timeline-date">2004年</p>
+    <div class="timeline-content">
+      <h3>シャンデリアラウンジ「　TEN　」</h3>
+      <p>開店</p>
+    </div>
+</li>
+<li>
+    <p class="timeline-date">2004年</p>
+    <div class="timeline-content">
+      <h3>港区西麻布にて中華料理レストラン「霞楼」</h3>
+      <p>事業承継し売却<br>
+デザイナーズレストラン
+</p>
+    </div>
+</li>
+<li>
+    <p class="timeline-date">2004年</p>
+    <div class="timeline-content">
+      <h3>渋谷西武A館地下セタンジュフロアー「セタンジュカフェ」</h3>
+      <p>事業承継し売却</p>
+    </div>
+</li>
+<li>
+    <p class="timeline-date">2007年</p>
+    <div class="timeline-content">
+      <h3>港区西麻布にて４階建て一棟飲食ビル開店</h3>
+      <p>1F「おすしやさん」
+2Fふぐ料理「タン」
+3Fカラオケルーム
+4Fガールズバー
+</p>
+    </div>
+</li>
+<li>
+    <p class="timeline-date">2008年</p>
+    <div class="timeline-content">
+      <h3>六本木にラウンジバー「ケセラパセラ」</h3>
+      <p>開店</p>
+    </div>
+</li>
+<li>
+    <p class="timeline-date">2008年</p>
+    <div class="timeline-content">
+      <h3>六本木に寿司店「プレミアム」</h3>
+      <p>開店</p>
+    </div>
+</li>
+<li>
+    <p class="timeline-date">2009年</p>
+    <div class="timeline-content">
+      <h3>中国上海にて和食居酒屋「たむら」</h3>
+      <p>コンサルティング</p>
+    </div>
+</li>
+<li>
+    <p class="timeline-date">2011年</p>
+    <div class="timeline-content">
+      <h3>港区西麻布に会員制ラウンジバー「カプア」</h3>
+      <p>居抜き事業承継</p>
+    </div>
+</li>
+<li>
+    <p class="timeline-date">2015年</p>
+    <div class="timeline-content">
+      <h3>中目黒　イタリアンレストラン「イゾラーニ」</h3>
+      <p>事業承継リニュアール開店</p>
+    </div>
+</li>
+<li>
+    <p class="timeline-date">2017年</p>
+    <div class="timeline-content">
+      <h3>代々木上原　バーラウンジ「ＣＯＲＥ」</h3>
+      <p>事業承継リニュアール開店</p>
+    </div>
+</li>
+<li>
+    <p class="timeline-date">2018年</p>
+    <div class="timeline-content">
+      <h3>中目黒にてバーラウンジ「ＲＵＢＹ」</h3>
+      <p>事業承継開店</p>
+    </div>
+</li>
+<li>
+    <p class="timeline-date">2019年</p>
+    <div class="timeline-content">
+      <h3>代々木にてバーM31</h3>
+      <p>事業承継開店</p>
+    </div>
+</li>
+<li>
+    <p class="timeline-date">2020年</p>
+    <div class="timeline-content">
+      <h3>渋谷にて神山町バー</h3>
+      <p>コンサルティング</p>
+    </div>
+</li>
+	
+	
+</ul>
+</div>
+</div>
+</section>
+	
+
+<section id="faq" class="section-contact section-padding onepage-section section-meta">
+<div class="container">
+<div class="section-title-area">
+<h2 class="section-title">よくある質問</h2>
+</div>
+<div class="section-content">
+	
+<div class="cp_qa">
+	<div class="cp_actab">
+		<input id="cp_tabfour041" type="checkbox" name="tabs">
+		<label for="cp_tabfour041"><span>相談にあたり費用はかかりますか？</span></label>
+		<div class="cp_actab-content">
+		<p>ご相談は一切無料です。</p>
+		</div>
+	</div>
+	<div class="cp_actab">
+		<input id="cp_tabfour042" type="checkbox" name="tabs">
+		<label for="cp_tabfour042"><span>相談はどのような流れになりますか？</span></label>
+		<div class="cp_actab-content">
+		<p>先ずはご相談内容をメールで送っていただき、その上で「助っ人」が回答をします。また、必要であれば直接ご面談にお伺いいたします。</p>
+		</div>
+	</div>
+	<div class="cp_actab">
+		<input id="cp_tabfour043" type="checkbox" name="tabs">
+		<label for="cp_tabfour043"><span>お店を閉めたくはありません、後継者がいなく承継していただけますか？</span></label>
+		<div class="cp_actab-content">
+		<p>場合によっては当社は承継先をご紹介いたします。</p>
+		</div>
+	</div>
+	
+	<div class="cp_actab">
+		<input id="cp_tabfour044" type="checkbox" name="tabs">
+		<label for="cp_tabfour044"><span>従業員には内密にしていただけますか？</span></label>
+		<div class="cp_actab-content">
+		<p>もちろん、一番重要なことですので秘密厳守いたします。</p>
+		</div>
+	</div>
+	
+</div>
+	
+</div>
+</div>
+</section>
+
+			<?php
+
+            do_action( 'screenr_frontpage_before_section_parts' );
+
+			if ( ! has_action( 'screenr_frontpage_section_parts' ) ) {
+
+				$sections = apply_filters (
+					'screenr_frontpage_sections_order',
+					array(
+                    	'slider',
+						'features',
+						'about',
+						'videolightbox',
+						
+						'services',
+						'clients',
+						'counter',
+						'news',
+						'contact'
+                	)
+				);
+
+				foreach ( $sections as $section ){
+                    // If  section not disable
+                    if ( ! get_theme_mod( $section.'_disable' ) ) {
+                        /**
+                         * Hook before section
+                         */
+                        do_action('screenr_before_section_' . $section);
+                        do_action('screenr_before_section_part', $section);
+
+                        /**
+                         * Load section template part
+                         */
+                        get_template_part('section-parts/section', $section);
+
+                        /**
+                         * Hook after section
+                         */
+                        do_action('screenr_after_section_part', $section);
+                        do_action('screenr_after_section_' . $section);
+                    }
+				}
+
+			} else {
+				do_action( 'screenr_frontpage_section_parts' );
+			}
+
+            do_action( 'screenr_frontpage_after_section_parts' );
+
+			?>			
+
+<section id="contact" class="section-contact section-padding onepage-section section-meta">
+<div class="container">
+<div class="section-title-area">
+<h2 class="section-title">無料相談フォーム</h2>
+</div>
+<div class="section-content">
+<div class="row">
+
+<div class="col-md-1"></div>
+				<div class="col-md-10">
+										<ul class="contact-details">
+													<li class="contact-detail">
+								<span class="contact-icon"><i aria-hidden="true" class="fa fa-phone fa-2x"></i></span>																	<span class="contact-detail-value">03-5784-3919</span>
+															</li>
+													<li class="contact-detail">
+								<span class="contact-icon"><i aria-hidden="true" class="fa fa-envelope fa-2x"></i></span>																	<span class="contact-detail-value">info@jigyousaiseikikou.com</span>
+															</li>
+											</ul>
+															<div class="contact-form-fields">
+						<div role="form" class="wpcf7" id="wpcf7-f6-o1" lang="ja" dir="ltr">
+<div class="screen-reader-response"><p role="status" aria-live="polite" aria-atomic="true"></p> <ul></ul></div>	
+
+	<?php
+  echo do_shortcode('[contact-form-7 id="6" title="コンタクトフォーム 1"]	');
+?>
+	
+			
+</div>
+</div>
+</div>
+</section>			
+			
+		</main><!-- #main -->
+	</div><!-- #content -->
+
+<?php get_footer(); ?>
